8559ceccc396b5bfbba8d8bd73ab32c4df3b439e
Doriana <133523673+DorianaPetkova@users.noreply.github.com>
2026-01-13 01:05:09 +0200

    Leaderboard implementation

--- Game2048/Game2048/Game2048.cpp
+++ Game2048/Game2048/Game2048.cpp
@@ -1,18 +1,15 @@
 ﻿/**
-*
 * Solution to course project #8
 * Introduction to programming course
 * Faculty of Mathematics and Informatics of Sofia University
 * Winter semester 2025/2026
-*
+
 * @author --
 * @idnumber ---
 * @compiler VC
-*
-* <Used as a Main - holds all of the code>
-*
-*/
 
+* <Used as a Main - holds all of the code>
+*/
 #include <iostream>
 #include <fstream>
 #include <ctime>
@@ -20,18 +17,15 @@
 // Global constants
 
 int board[10][10];
-int N; // Size of the board
+int N;
 char username[101];
 
 // declaration of functions
 
 void mainMenu();
 void StartNewGame();
-void GameLoop();
-void LoadGame() {};
-void SaveGame() {};
 void Instructions();
-void Leaderboards() {};
+void Leaderboards();
 void ExitGame() {};
 
 void StartFormat();
@@ -41,27 +35,35 @@ bool MoveRight();
 bool MoveUp();
 bool MoveDown();
 bool ChooseMove(char move);
+void ReverseRow(int row[10]);
+void ReverseColumn(int col[10]);
+
+void GameLoop();
+void CompressRowLeft(int row[10]);
 
 bool IsBoardEqual(int board1[10][10], int board2[10][10]);
 void CopyBoard(int source[10][10], int destination[10][10]);
-void CompressRowLeft(int row[10]);
 void PrintBoard();
+void PrintHeader();
+void PrintControls();
+
+void StrCopy(char dest[], const char src[]);
+void SaveToLeaderboard(int movesCount);
 
 void GenerateRandomTile();
 int CheckLevelTile();
 int FindMaxTileValue();
 
-void PrintHeader();
-void PrintControls();
 bool HandleCommands(char move, int& movesCount);
-void HandleGameOver();
-
+void HandleGameOver(int movesCount);
+void SaveToLeaderboard(int movesCount);
 
 int main()
 {
 	srand(time(0));
-	mainMenu();
-	N = 4;
+	
+
+	Leaderboards();
 	return 0;
 }
 
@@ -80,15 +82,12 @@ void mainMenu()
 			StartNewGame();
 			break;
 		case 2:
-			LoadGame();
-			break;
-		case 3:
 			Instructions();
 			break;
-		case 4:
+		case 3:
 			Leaderboards();
 			break;
-		case 5:
+		case 4:
 			ExitGame();
 			break;
 		default:
@@ -105,10 +104,9 @@ void StartFormat()
 	std::cout << "  |    2048 GAME    |\n";
 	std::cout << "  -------------------\n\n";
 	std::cout << "1. Start New Game\n";
-	std::cout << "2. Load Game\n";
-	std::cout << "3. Instructions\n";
-	std::cout << "4. Leaderboards\n";
-	std::cout << "5. Exit\n";
+	std::cout << "2. Instructions\n";
+	std::cout << "3. Leaderboards\n";
+	std::cout << "4. Exit\n";
 	std::cout << "\n  ==================\n\n";
 	std::cout << "  >> ";
 }
@@ -147,42 +145,37 @@ void CopyBoard(int source[10][10], int destination[10][10])
 void PrintBoard() {
 	for (int i = 0; i < N; i++) {
 		for (int j = 0; j < N; j++) {
-			std::cout << board[i][j] << "\t";
-		}
+			std::cout << board[i][j] << "\t";}
 		std::cout << "\n";
 	}
 }
 
-
 void CompressRowLeft(int row[10])
 {
-	int temp[10] = { 0 };
-	int index = 0;
-	for (int j = 0; j < N; j++) {
-		if (row[j] != 0) {
-			temp[index++] = row[j];
-		}
-	}
-	
-	for (int j = 0; j < N - 1; j++)
-	{
-		if (temp[j] != 0 && temp[j] == temp[j + 1])
-		{
-			temp[j] *= 2;
-			temp[j + 1] = 0;
-		}
-	}
-		for (int j = 0; j < N; j++)
-			row[j] = 0;
+	int result[10] = { 0 };
+	int write = 0;
 
-		index = 0;
-		for (int j = 0; j < N; j++) {
-			if (temp[j] != 0) {
-				row[index++] = temp[j];
-			}
+	for (int i = 0; i < N; i++) {
+		if (row[i] == 0)
+			continue;
+
+		if (result[write] == 0) {
+			result[write] = row[i];
 		}
-	
+		else if (result[write] == row[i]) {
+			result[write] *= 2;
+			write++;
+		}
+		else {
+			write++;
+			result[write] = row[i];
+		}
+	}
+
+	for (int i = 0; i < N; i++)
+		row[i] = result[i];
 }
+
 bool MoveLeft()
 {
 	int oldBoard[10][10];
@@ -194,24 +187,33 @@ bool MoveLeft()
 	return !IsBoardEqual(board, oldBoard);
 }
 
+void ReverseRow(int row[10])
+{
+	for (int i = 0; i < N / 2; i++) {
+		int tmp = row[i];
+		row[i] = row[N - 1 - i];
+		row[N - 1 - i] = tmp;
+	}
+}
+
+void ReverseColumn(int col[10])
+{
+	for (int i = 0; i < N / 2; i++) {
+		int tmp = col[i];
+		col[i] = col[N - 1 - i];
+		col[N - 1 - i] = tmp;
+	}
+}
+
 bool MoveRight()
 {
 	int oldBoard[10][10];
 	CopyBoard(board, oldBoard);
+
 	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < N / 2; j++) {
-			int tmp = board[i][j];
-			board[i][j] = board[i][N - 1 - j];
-			board[i][N - 1 - j] = tmp;
-		}
-
-			CompressRowLeft(board[i]);
-
-		for (int j = 0; j < N / 2; j++) {
-			int tmp = board[i][j];
-			board[i][j] = board[i][N - 1 - j];
-			board[i][N - 1 - j] = tmp;
-		}
+		ReverseRow(board[i]);
+		CompressRowLeft(board[i]);
+		ReverseRow(board[i]);
 	}
 	return !IsBoardEqual(board, oldBoard);
 }
@@ -237,25 +239,19 @@ bool MoveDown()
 {
 	int oldBoard[10][10];
 	CopyBoard(board, oldBoard);
+
 	for (int j = 0; j < N; j++) {
 		int column[10];
-		for (int i = 0; i < N; i++) {
+
+		for (int i = 0; i < N; i++)
 			column[i] = board[i][j];
-		}
-		for (int i = 0; i < N / 2; i++) {
-			int tmp = column[i];
-			column[i] = column[N - 1 - i];
-			column[N - 1 - i] = tmp;
-		}
+
+		ReverseColumn(column);
 		CompressRowLeft(column);
-		for (int i = 0; i < N / 2; i++) {
-			int tmp = column[i];
-			column[i] = column[N - 1 - i];
-			column[N - 1 - i] = tmp;
-		}
-		for (int i = 0; i < N; i++) {
+		ReverseColumn(column);
+
+		for (int i = 0; i < N; i++)
 			board[i][j] = column[i];
-		}
 	}
 	return !IsBoardEqual(board, oldBoard);
 }
@@ -289,6 +285,7 @@ int FindMaxTileValue()
 	}
 	return maxTileValue;
 }
+
 int CheckLevelTile()
 {
 	int maxTileValue = FindMaxTileValue(); 
@@ -298,7 +295,7 @@ int CheckLevelTile()
 	if (maxTileValue >= 512)
 	{
 		if (r < 50)
-			value = 4;
+			value = 4; 
 		else if (r < 90)
 			value = 2;
 		else
@@ -377,7 +374,6 @@ void PrintControls()
 	std::cout << "  a = Move Left\n";
 	std::cout << "  s = Move Down\n";
 	std::cout << "  d = Move Right\n";
-	std::cout << "  v = Save Game\n";
 	std::cout << "  q = Quit to Menu\n";
 	std::cout << "===================================\n";
 	std::cout << "Enter move: ";
@@ -416,16 +412,20 @@ bool CheckGameOver() {
 	return true;
 }
 
-void HandleGameOver() {
+void HandleGameOver(int movesCount) {
 	std::cout << "\n GAME OVER! No moves possible.\n";
 	std::cout << "Final Score: " << FindMaxTileValue() << "\n";
+	SaveToLeaderboard(movesCount);
 	std::cout << "\nPress Enter to return to main menu...";
 	std::cin.ignore();
 	std::cin.get();
 }
 
 bool HandleCommands(char move, int& movesCount) {
-	//implement quit and save
+	if (move == 'q' || move == 'Q') {
+		std::cout << "\nReturning to main menu...\n";
+		return true;
+	}
 
 	if (ChooseMove(move)) {
 		movesCount++;
@@ -447,37 +447,140 @@ void GameLoop() {
 	char move;
 	int movesCount = 0;
 	PrintHeader();
-	
+
 	while (true) {
 		PrintBoard();
-		std::cout << "\n";
-		std::cout << "Max Tile: " << FindMaxTileValue() << "\n";
-		std::cout << "Moves: " << movesCount << "\n";
-
-		bool hasEmptyCell = false;
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < N; j++) {
-				if (board[i][j] == 0) {
-					hasEmptyCell = true;
-					break;
-				}
-			}
-			if (hasEmptyCell) break;
-		}
-
+		std::cout << "\nMax Tile: " << FindMaxTileValue()
+			<< "\nMoves: " << movesCount << "\n";
 		if (CheckGameOver()) {
-			HandleGameOver();
+			HandleGameOver(movesCount);
 			return;
 		}
-
 		PrintControls();
 		std::cin >> move;
-		std::cin.ignore();  
+		std::cin.ignore();
 
-		if (HandleCommands(move, movesCount)) {
-			return; 
-		}
-
+		if (HandleCommands(move, movesCount))
+			return;
 		std::cout << "\n";
 	}
 }
+
+void StrCopy(char dest[], const char src[]) {
+	int i = 0;
+	while (src[i] != '\0') {
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+}
+
+int StrCmp(char a[], char b[]) {
+	int i = 0;
+	while (a[i] != '\0' && b[i] != '\0') {
+		if (a[i] != b[i])
+			return a[i] - b[i];
+		i++;
+	}
+	return a[i] - b[i];
+}
+
+void swap(int& first, int& second) {
+	int temp = first;
+	first = second;
+	second = temp;
+}
+
+void SwapStr(char a[], char b[]) {
+	char tmp[101];
+	StrCopy(tmp, a);
+	StrCopy(a, b);
+	StrCopy(b, tmp);
+}
+
+void SaveToLeaderboard(int movesCount)
+{
+	char filename[32];
+	if (N < 10) {
+		filename[0] = '0' + N;
+		filename[1] = 'x';
+		filename[2] = '0' + N;
+		filename[3] = '_';
+		const char text[] = "leaderboard.txt";
+		int pos = 4;
+		for (int i = 0; text[i] != '\0'; i++)
+			filename[pos++] = text[i];
+		filename[pos] = '\0';
+	}
+	else { 
+		filename[0] = '1';
+		filename[1] = '0';
+		filename[2] = 'x';
+		filename[3] = '1';
+		filename[4] = '0';
+		filename[5] = '_';
+		const char text[] = "leaderboard.txt";
+		int pos = 6;
+		for (int i = 0; text[i] != '\0'; i++)
+			filename[pos++] = text[i];
+		filename[pos] = '\0';
+	}
+
+	
+	std::ofstream file(filename, std::ios::app);
+	if (!file.is_open()) {
+		std::cout << "Error opening leaderboard file!\n";
+		return;
+	}
+
+	file << username << " " << FindMaxTileValue() << " " << movesCount << "\n";
+	file.close();
+	std::cout << "Saved " << username << " to " << filename << "\n";
+}
+
+
+
+void Leaderboards() {
+	std::ifstream file("4x4_leaderboard.txt");
+    if (!file.is_open()) {
+        return; }
+
+    std::cout << "===== LEADERBOARD 4x4 =====\n";
+    std::cout << "Name\tScore\tMoves\n";
+
+
+    char line[256];
+    int count = 0;
+    while (file.getline(line, 256)) {
+        char name[101];
+        int score = 0, moves = 0;
+        int i = 0, j = 0;
+
+       
+        while (line[i] != ' ' && line[i] != '\0' && j < 100) {
+            name[j++] = line[i++];
+        }
+        name[j] = '\0';
+        if (line[i] == ' ') i++;
+        score = 0;
+        while (line[i] >= '0' && line[i] <= '9') {
+            score = score * 10 + (line[i] - '0');
+            i++;
+        }
+        if (line[i] == ' ') i++;
+        moves = 0;
+        while (line[i] >= '0' && line[i] <= '9') {
+            moves = moves * 10 + (line[i] - '0');
+            i++;
+        }
+        std::cout << name << "\t" << score << "\t" << moves << "\n";
+
+        count++;
+        if (count >= 5) break;
+    }
+
+    file.close();
+    std::cin.ignore();
+    std::cin.get();
+}
+
